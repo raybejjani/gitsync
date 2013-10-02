92f91bcfa6990045fa861772c0e8b11ec6c2316c
Ray Bejjani <ray.bejjani@gmail.com>
2013-10-01 20:47:17 -0700

    CLI options to control logging to socket and file, and log level at CLI
    Log output can be configured to go to a file or a socket. The two can be used at
    the same time. The level is also configurable. By default the socket is set to
    the system default syslog endpoint. This leaves stdout free to report seen
    changes.
    Note: OSX seems to make it impossible to see anything below Error on syslog.

--- src/gitsyncd/daemon.go
+++ src/gitsyncd/daemon.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	log "github.com/ngmoco/timber"
@@ -9,6 +10,7 @@ import (
 	"os"
 	"os/signal"
 	"os/user"
+	"strings"
 	"time"
 )
 
@@ -52,6 +54,95 @@ func Clone(source chan gitsync.GitChange) (duplicate chan gitsync.GitChange) {
 	return
 }
 
+// unixSyslog discovers where the syslog daemon is running on the local machine
+// using a Unix domain socket.
+// NOTE: Stolen from golang log/syslog/syslog_unix.go
+func unixSyslog() (network, addr string, err error) {
+	logTypes := []string{"unixgram", "unix"}
+	logPaths := []string{"/dev/log", "/var/run/syslog"}
+	for _, network := range logTypes {
+		for _, path := range logPaths {
+			conn, err := net.Dial(network, path)
+			if err != nil {
+				continue
+			} else {
+				defer conn.Close()
+				return conn.RemoteAddr().Network(), conn.RemoteAddr().String(), nil
+			}
+		}
+	}
+	return "", "", errors.New("Unix syslog delivery error")
+}
+
+// setupLogging initialises logging per the parameters:
+// logLevel: lowest level to log
+// logSocket: socket to log to (defaults to the system syslog)
+// logFile: file to log to
+func setupLogging(logLevel, logSocket, logFile string) (err error) {
+	var (
+		network string      // the network type to pass Dial
+		address string      // the address to Dial to
+		level   = log.DEBUG // the lowest level to log
+	)
+
+	if logLevel != "" {
+		level = log.Level(0)
+		for idx, str := range log.LongLevelStrings {
+			if strings.EqualFold(str, logLevel) {
+				level = log.Level(idx)
+			}
+		}
+		if level == log.Level(0) {
+			return fmt.Errorf("Cannot parse log level %s", logLevel)
+		}
+	}
+
+	if logSocket == "" {
+		if network, address, err = unixSyslog(); err != nil {
+			return
+		}
+	} else {
+		parts := strings.Split(logSocket, "://")
+		if len(parts) < 2 {
+			return fmt.Errorf("Cannot parse log socket %s", logSocket)
+		}
+		network, address = parts[0], parts[1]
+	}
+
+	if network != "" && address != "" {
+		var writer log.LogWriter
+		if writer, err = log.NewSocketWriter(network, address); err != nil {
+			return err
+		}
+		// add console output for logs
+		log.AddLogger(log.ConfigLogger{
+			LogWriter: writer,
+			Level:     level,
+			Formatter: log.NewSyslogFormatter("[%L] %s %M"),
+		})
+	}
+
+	if logFile != "" {
+		var writer log.LogWriter
+		if writer, err = log.NewFileWriter(logFile); err != nil {
+			return err
+		}
+		log.AddLogger(log.ConfigLogger{
+			LogWriter: writer,
+			Level:     level,
+			Formatter: log.NewPatFormatter("[%D %T][%L] %s %M"),
+		})
+	}
+
+	return
+}
+
+// fatalf logs a fatal error and exits
+func fatalf(format string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, format+"\n", args...)
+	log.Fatalf(format, args...)
+}
+
 func ReceiveChanges(changes chan gitsync.GitChange) {
 	for {
 		select {
@@ -61,34 +152,34 @@ func ReceiveChanges(changes chan gitsync.GitChange) {
 				break
 			}
 
-			log.Info("saw %+v", change)
+			log.Info("%+v", change)
+			fmt.Printf("%+v\n", change)
 		}
 	}
 }
 
-func init() {
-	// add console output for logs
-	log.AddLogger(log.ConfigLogger{
-		LogWriter: new(log.ConsoleWriter),
-		Level:     log.DEBUG,
-		Formatter: log.NewPatFormatter("[%D %T] [%L] %S %M"),
-	})
-}
-
 func main() {
-	log.Debug("Starting up")
-
 	// Start changes handler
 	var (
 		username  = flag.String("user", "", "Username to report when sending changes to the network")
 		groupIP   = flag.String("ip", gitsync.IP4MulticastAddr.IP.String(), "Multicast IP to connect to")
 		groupPort = flag.Int("port", gitsync.IP4MulticastAddr.Port, "Port to use for network IO")
+		logLevel  = flag.String("loglevel", "info", "Lowest log level to emit. Can be one of debug, info, warning, error.")
+		logSocket = flag.String("logsocket", "", "proto://address:port target to send logs to")
+		logFile   = flag.String("logfile", "", "path to file to log to")
 	)
 	flag.Parse()
 
 	if len(flag.Args()) == 0 {
-		log.Fatalf("No Git directory supplied")
+		fatalf("No Git directory supplied")
 	}
+
+	if err := setupLogging(*logLevel, *logSocket, *logFile); err != nil {
+		fatalf("Cannot setup logging: %s", err)
+	}
+
+	log.Info("Starting up")
+	defer log.Info("Exiting")
 
 	var (
 		err       error
@@ -109,20 +200,17 @@ func main() {
 	} else if user, err := user.Current(); err == nil {
 		netName = user.Username
 	} else {
-		log.Critical("Cannot get username: %v", err)
-		os.Exit(1)
+		fatalf("Cannot get username: %v", err)
 	}
 
 	if groupAddr, err = net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%d", *groupIP, *groupPort)); err != nil {
-		log.Critical("Cannot resolve address %v:%v: %v", *groupIP, *groupPort, err)
-		os.Exit(1)
+		fatalf("Cannot resolve address %v:%v: %v", *groupIP, *groupPort, err)
 	}
 
 	// start directory poller
 	repo, err := gitsync.NewCliRepo(dirName)
 	if err != nil {
-		log.Critical("Cannot open repo: %s", err)
-		os.Exit(1)
+		fatalf("Cannot open repo: %s", err)
 	}
 	go gitsync.PollDirectory(log.Global, dirName, repo, localChanges, 1*time.Second)
 
@@ -136,6 +224,4 @@ func main() {
 	s := make(chan os.Signal, 1)
 	signal.Notify(s, os.Kill)
 	<-s
-
-	log.Debug("Exiting")
 }
