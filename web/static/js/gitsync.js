296e0014b8b2f1b1b88ba5b6520fbd6aa88dc911
Ghalib Suleiman <ghalib@sent.com>
2013-10-11 13:07:25 -0700

    Added timestamp to statuses
    Closes #19

--- web/static/js/gitsync.js
+++ web/static/js/gitsync.js
@@ -1,14 +1,21 @@
+function currentTimestamp() {
+    function pad(n){return n < 10 ? '0' + n : n;}
+    var date = new Date();
+    return pad(date.getHours()) + ':'
+        + pad(date.getMinutes());
+}
+
 $(function() {
     var ws = new WebSocket('ws://localhost:12345/events');
     ws.onmessage = function (evt) {
         var gitChange = JSON.parse(evt.data);
-        console.log(JSON.stringify(gitChange));
         var user = gitChange['User'];
         var refName = gitChange['RefName'];
         var checkedOut = gitChange['CheckedOut'];
+        var timestamp = currentTimestamp();
         if (checkedOut) {
             $('<div class="status">' + user + ' modified ' + 
-              refName + '</div>')
+              refName + ' <span class="timestamp">' + timestamp + '</span></div>')
                 .hide()
                 .appendTo('.container')
                 .fadeIn(1000);
