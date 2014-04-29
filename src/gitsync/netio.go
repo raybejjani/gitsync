4ef5cc60093d533b5d6679674327f7834a6aa99d
Ray Bejjani <ray.bejjani@gmail.com>
2014-04-29 14:32:45 -0700

    Refactored gitsync.NetIO to separate out concerns

--- src/gitsync/netio.go
+++ src/gitsync/netio.go
@@ -20,16 +20,65 @@ var (
 	}
 )
 
+// init registers GitChange with gob to ensure we can send it on the network.
 func init() {
 	gob.Register(GitChange{})
 }
 
+// establishConnPair builds a send/receive connection pair to work with the
+// multicast network at address addr
 func establishConnPair(addr *net.UDPAddr) (recvConn, sendConn *net.UDPConn, err error) {
 	if recvConn, err = net.ListenMulticastUDP("udp", nil, addr); err != nil {
 		return
 	}
 
 	if sendConn, err = net.DialUDP("udp", nil, addr); err != nil {
+		return
+	}
+
+	return
+}
+
+// networkToChannel loops, reading packets of the network and deserialising them
+// then places them onto out
+func networkToChannel(l log.Logger, conn *net.UDPConn, out chan GitChange) {
+	for {
+		var (
+			rawPacket = make([]byte, 1024)
+			reader    = bytes.NewReader(rawPacket)
+			dec       = gob.NewDecoder(reader)
+		)
+
+		if _, err := conn.Read(rawPacket); err != nil {
+			l.Critical("Cannot read socket: %s", err)
+			return
+		}
+
+		var change GitChange
+
+		if err := dec.Decode(&change); err != nil {
+			l.Critical("%s", err)
+			continue
+		}
+
+		l.Debug("received %+v", change)
+		out <- change
+	}
+}
+
+// sendChangeToNetwork encodes and sends a GitChange to the conn (assumed to be
+// connected).
+func sendChangeToNetwork(l log.Logger, conn *net.UDPConn, change *GitChange) (bytesWritten int, err error) {
+	l.Info("Sending %+v", change)
+	buf := bytes.Buffer{}
+	enc := gob.NewEncoder(&buf)
+
+	if err = enc.Encode(change); err != nil {
+		return
+	}
+
+	l.Fine("Sending %+v", buf.Bytes())
+	if bytesWritten, err = conn.Write(buf.Bytes()); err != nil {
 		return
 	}
 
@@ -44,6 +93,9 @@ func NetIO(l log.Logger, repo Repo, addr *net.UDPAddr, fromNet, toNet chan GitCh
 		err                error
 		recvConn, sendConn *net.UDPConn // UDP connections to allow us to send and receive change updates
 		peerAddr           string       // the address we will put into network-bound changes
+		// decodeFromNet is an intermediate channel for packets from the network. We
+		// do some checks on them before we pass them onto toNet
+		decodedFromNet = make(chan GitChange, cap(fromNet))
 	)
 
 	l.Info("Joining %v multicast(%t) group", addr, addr.IP.IsMulticast())
@@ -57,58 +109,31 @@ func NetIO(l log.Logger, repo Repo, addr *net.UDPAddr, fromNet, toNet chan GitCh
 	defer sendConn.Close()
 	peerAddr = sendConn.LocalAddr().(*net.UDPAddr).String()
 
-	term := false
-	defer func() { term = true }()
-	rawFromNet := make(chan []byte, 128)
-	go func() {
-		for !term {
-			b := make([]byte, 1024)
-
-			if n, err := recvConn.Read(b); err != nil {
-				l.Critical("Cannot read socket: %s", err)
-				continue
-			} else {
-				rawFromNet <- b[:n]
-			}
-		}
-	}()
+	go networkToChannel(l, recvConn, decodedFromNet)
 
 	for {
 		select {
-		case req, ok := <-toNet:
+		case change, ok := <-toNet:
 			// exit when this channel is closed
 			if !ok {
 				return
 			}
 
-			req.PeerAddr = peerAddr
+			// Drop requests that have a PeerAddr since that means they were on the
+			// network already (and shared by us locally).
+			if change.IsFromNetwork() {
+				continue
+			}
 
-			l.Info("Sending %+v", req)
-			buf := &bytes.Buffer{}
-			enc := gob.NewEncoder(buf)
+			// Add fields that matter for the network
+			change.PeerAddr = peerAddr
 
-			if err := enc.Encode(req); err != nil {
+			if _, err := sendChangeToNetwork(l, sendConn, &change); err != nil {
 				l.Critical("%s", err)
 				continue
 			}
 
-			l.Fine("Sending %+v", buf.Bytes())
-			if _, err := sendConn.Write(buf.Bytes()); err != nil {
-				l.Critical("%s", err)
-				continue
-			}
-
-		case resp := <-rawFromNet:
-			var change GitChange
-			dec := gob.NewDecoder(bytes.NewReader(resp))
-
-			if err := dec.Decode(&change); err != nil {
-				l.Critical("%s", err)
-				continue
-			} else {
-				l.Debug("received %+v", change)
-			}
-
+		case change := <-decodedFromNet:
 			if rootCommit, err := repo.RootCommit(); err != nil {
 				log.Critical("Error getting root commit")
 			} else {
